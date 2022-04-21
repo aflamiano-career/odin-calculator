dae88cabbcc68258d82b11fc3d01efcdacc90ea2
aflamiano <aflamiano.career@gmail.com>
2022-04-21 20:53:47 +0800

    added keyboard functionality and fixes

--- scripts.js
+++ scripts.js
@@ -30,11 +30,6 @@ const operate = (operator, num1, num2) => {
     }
 }
 
-// console.log(operate('/', 1, 1));
-// console.log(operate('*', 1, 2));
-// console.log(operate('-', 1, 2));
-// console.log(operate('+', 1, 2));
-
 const displayedMemory = document.querySelector('.js-display-memory');
 const displayedTotal = document.querySelector('.js-display-total');
 const displayedInput = document.querySelector('.js-display-input');
@@ -48,8 +43,7 @@ let display = 0;
 const deleteFunction = () => {
     inputs.pop();
     if (!inputs.length) {
-        display = 0;
-        displayedInput.innerText = display;
+        clearFunction();
     } else {
         display = inputs.join('');
         displayedInput.innerText = display;
@@ -71,57 +65,79 @@ const buttonClear = document.querySelector('.btn-clear');
 buttonClear.addEventListener('click', clearFunction);
 
 const isNumber = (value) => {
-    return value.match(/[0-9]/g);
+    return value.match(/[0-9]/g) ? true : false;
+}
+
+const calculate = (keyValue) => {
+    if (isNumber(keyValue)) {
+        inputs.push(keyValue);
+        display = inputs.join('');
+    } else if (keyValue === '.') {
+        let decimal = inputs
+                            .join('')
+                            .split(/[^0-9\.]/g);
+        if (!decimal[decimal.length-1].includes('.')) {
+            inputs.push(keyValue);
+            display = inputs.join('');
+        }
+    } else if (keyValue === '=' || keyValue === 'Enter') {
+        if (!inputs.length) {
+            // DO NOTHING
+        } else if (isNumber(inputs[inputs.length-1])) {
+            let numberSets = inputs
+                            .join('')
+                            .split(/[^0-9\.]/g);
+            let operations = inputs.filter((input) => {
+                return !isNumber(input) && input !== '.';
+            });
+            let result = numberSets.reduce((result, numbers, index) => {
+                return operate(operations[index - 1], parseFloat(result), parseFloat(numbers));
+            });
+            if (isNaN(result)) {
+                clearFunction();
+                displayedTotal.innerText = result;
+            } else {
+                displayedTotal.innerText = Math.round(result * 100) / 100;
+            }
+            displayedMemory.innerText = inputs.join('');
+        } else {
+            alert('Malformed expression.');
+        }
+    } else {
+        if (inputs.length) { 
+            if (isNumber(inputs[inputs.length-1])) {
+                inputs.push(keyValue);
+            } else {
+                inputs[inputs.length-1] = keyValue;
+            }
+        }
+        display = inputs.join('');
+    }
+    displayedInput.innerText = display;
 }
 
 mainKeys.forEach((key) => {
     key.addEventListener('click', (e) => {
         let keyValue = e.target.value;
-        // console.log(keyValue);
-        if (isNumber(keyValue)) {
-            inputs.push(keyValue);
-            display = inputs.join('');
-        } else if (keyValue === '.') {
-            let decimal = inputs
-                                .join('')
-                                .split(/[^0-9.]/g);
-            if (!decimal[decimal.length-1].includes('.')) {
-                inputs.push(keyValue);
-                display = inputs.join('');
-            }
-        } else if (keyValue === '=') {
-            if (!inputs.length) {
-                // DO NOTHING
-            } else if (isNumber(inputs[inputs.length-1])) {
-                let numberSets = inputs
-                                .join('')
-                                .split(/[^0-9.]/g);
-                let operations = inputs.filter((input) => {
-                    return !isNumber(input) && input !== '.';
-                });
-                let result = numberSets.reduce((result, numbers, index) => {
-                    return operate(operations[index - 1], parseFloat(result), parseFloat(numbers));
-                });
-                // console.log(numberSets);
-                // console.log(operations);
-                // console.log(result);
-                if (isNaN(result)) {
-                    clearFunction();
-                    displayedTotal.innerText = result;
-                } else {
-                    displayedTotal.innerText = Math.round(result * 100) / 100;
-                }
-                displayedMemory.innerText = inputs.join('');
-            } else {
-                alert('Malformed expression.');
-            }
-        } else {
-            if (inputs.length && 
-                isNumber(inputs[inputs.length-1])) {
-                    inputs.push(keyValue);
-                    display = inputs.join('');
-            }
-        }
-        displayedInput.innerText = display;
+        calculate(keyValue);
     });
 });
+
+window.addEventListener('keydown', (e) => {
+    let keyValue = e.key;
+    switch(keyValue) {
+        case 'Backspace':
+            deleteFunction();
+            break;
+        case 'Delete':
+            clearFunction();
+        default:
+            if (keyValue.match(/[0-9\.\/\*\-\+]|Enter/g)) {
+                if(keyValue === '/' || keyValue === 'Enter') {
+                    e.preventDefault();
+                }
+                calculate(keyValue);
+            }
+            break;
+    }
+})
