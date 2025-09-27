c1e8beb724b9238a08a5a4205c38e4d7994d5194
aflamiano-career <aflamiano-career@gmail.com>
2025-09-28 07:59:59 +0800

    Restructure event handler behavior
    Capture num1 and num2 on digit button clicks
    Capture operator on operator button clicks
    Refine equal button behavior to handle operations correctly

--- script.js
+++ script.js
@@ -7,42 +7,55 @@ let expression = "";
 let output = "";
 let result = "";
 
-let operatorFlag = null;
-
+let hasOperator = null;
+let hasFirstNum = null;
 const outputScreen = document.querySelector(".screen p");
 
 const digitBtns = document.querySelectorAll(".digit-row button");
 digitBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    let digitValue = e.target.textContent;
-    output += digitValue;
+    output += e.target.textContent;
+
+    if (!hasFirstNum) {
+      num1 = parseFloat(output);
+    } else {
+      num2 = parseFloat(output);
+    }
 
     outputScreen.textContent = output;
-    operatorFlag = false;
+    hasOperator = false;
   });
 });
 
 const operatorBtns = document.querySelectorAll(".operator-btn");
 operatorBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    let operatorValue = e.target.textContent.toLowerCase();
-    operator = operatorValue;
+    operator = e.target.textContent.toLowerCase();
 
-    if (!operatorFlag) {
-      if (!num1) {
-        num1 = parseFloat(outputScreen.textContent);
-      } else {
-        num2 = parseFloat(outputScreen.textContent);
+    if (num1) {
+      hasFirstNum = true;
+    }
+
+    if (!hasOperator) {
+      if (num2) {
         num1 = operate(previousOperator, num1, num2);
         outputScreen.textContent = num1;
       }
     }
 
     previousOperator = operator;
-    operatorFlag = true;
-
+    hasOperator = true;
     output = "";
   });
+});
+
+const equalBtn = document.querySelector(".equal-btn");
+equalBtn.addEventListener("click", (e) => {
+  if (num2) {
+    num1 = operate(previousOperator, num1, num2);
+    outputScreen.textContent = num1;
+    hasOperator = true;
+  }
 });
 
 function add(num1, num2) {
@@ -74,13 +87,3 @@ function operate(operator, num1, num2) {
       break;
   }
 }
-
-// console.log(add(num1, num2));
-// console.log(subtract(num1, num2));
-// console.log(multiply(num1, num2));
-// console.log(divide(num1, num2));
-
-// console.log(operate("+", num1, num2));
-// console.log(operate("-", num1, num2));
-// console.log(operate("*", num1, num2));
-// console.log(operate("/", num1, num2));
